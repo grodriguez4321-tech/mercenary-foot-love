d7e00672b0429e9a8c895d18f7de352d726c759c
grodriguez4321-tech <grodriguez4321@gmail.com>
2026-09-03 05:44:20 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+class Merc:
+	def __init__(self, name, ancestry, profession, archetype, background, age, attributes, characteristics, skills, inventory)
+	self.name = name
+	self.ancestry = ancestry
+	self.profession = profession
+	self.archetype = archetype
+	self.background = background
+	self.age = age
+	self.attributes = attributes
+	self.characterisrics = characteristics
+	self.skills = skills
+	self.inventory = []
+	
+	
+class Ancestry:
+	def __init__(self, name, com_low, com_max, int_low, int_max, pro_low, pro_max, vig_low, vig_max)
+	self.name = name
