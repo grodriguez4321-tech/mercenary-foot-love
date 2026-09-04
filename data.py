a82e4f933bf995352c1fa01bdf38ae66b155830c
grodriguez4321-tech <grodriguez4321@gmail.com>
2026-09-04 14:26:12 -0500

    make

--- data.py
+++ data.py
@@ -1,17 +1,25 @@
 class Merc:
-	def __init__(self, name, ancestry, profession, archetype, background, age, attributes, characteristics, skills, inventory)
-	self.name = name
-	self.ancestry = ancestry
-	self.profession = profession
-	self.archetype = archetype
-	self.background = background
-	self.age = age
-	self.attributes = attributes
-	self.characterisrics = characteristics
-	self.skills = skills
-	self.inventory = []
+	def __init__(self, name, ancestry, profession, archetype, background, age, attributes, characteristics, skills, inventory):
+		self.name = name
+		self.ancestry = ancestry
+		self.profession = profession
+		self.archetype = archetype
+		self.background = background
+		self.age = age
+		self.attributes = attributes
+		self.characterisrics = characteristics
+		self.skills = skills
+		self.inventory = []
 	
 	
 class Ancestry:
-	def __init__(self, name, com_low, com_max, int_low, int_max, pro_low, pro_max, vig_low, vig_max)
-	self.name = name
+	def __init__(self, name, ancestry_attributes):
+		self.name = name
+		self.ancestry_attributes = ancestry_attributes
+clever = Ancestry('Clever', )
+clever_attributes = {
+	'composure' : 6,
+	'intellect' : 7,
+	'prowess' : 4,
+	'vigor' : 4
+}
